f205a5e82e2b26f2913d41713cb38d25fc84f9ae
Clementine Hassouni <clementine.hassouni@mymail.champlain.edu>
2022-03-24 01:22:21 -0400

    Adding sword slash shader/particle, adding sword slash sound, adding better animation trigger control

--- Assets/Scripts/CombatController.cs
+++ Assets/Scripts/CombatController.cs
@@ -4,26 +4,65 @@ using UnityEngine;
 
 public class CombatController : MonoBehaviour
 {
+    public List<Effect> effects;
+    public GameObject weapon;
+
     private StarterAssets.ThirdPersonController tpc;
+    private bool attacking = false;
+    private AudioSource audioSource;
 
     void Start()
     {
         tpc = GetComponent<StarterAssets.ThirdPersonController>();
+        audioSource = GetComponent<AudioSource>();
+        DisableEffects();
     }
 
     void Update()
     {
-        if (tpc._input.attack)
+        if (tpc._input.attack && !attacking)
         {
-            tpc._animator.SetBool("StartAttack", true);
-            
-            if (tpc._animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.95)
-            {
-                tpc._input.attack = false;
-                tpc._animator.SetBool("StartAttack", false);
-            }
+            attacking = true;
+            tpc._animator.SetTrigger("Attack");
+            StartCoroutine(EffectAttack());
+            StartCoroutine(EffectAudio());
         }
     }
 
+    IEnumerator EffectAudio()
+    {
+        for (int i = 0; i < effects.Count; i++)
+        {
+            yield return new WaitForSeconds(effects[i].audioDelay);
+            audioSource.PlayOneShot(effects[i].audio);
+        }
+    }
+    IEnumerator EffectAttack()
+    {
+        for (int i = 0; i < effects.Count; i++)
+        {
+            yield return new WaitForSeconds(effects[i].effectDelay);
+            effects[i].effectObject.SetActive(true);
+        }
 
+        yield return new WaitForSeconds(1);
+        DisableEffects();
+        tpc._input.attack = attacking = false;
+        tpc._animator.ResetTrigger("Attack");
+    }
+
+    private void DisableEffects()
+    {
+        for (int i = 0; i < effects.Count; i++)
+            effects[i].effectObject.SetActive(false);
+    }
+
+}
+
+[System.Serializable]
+public class Effect
+{
+    public GameObject effectObject;
+    public float effectDelay, audioDelay;
+    public AudioClip audio;
 }
